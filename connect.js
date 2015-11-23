48251445f7df4b6cb30db0d9241c1ed37d7a7e17
1400003 <1400003@rgu.ac.uk>
2015-11-23 16:18:27 +0000

    Hello

--- connect.js
+++ connect.js
@@ -6,3 +6,19 @@ var http = require ('http');
 var port = process.env.PORT || 1337;
 var url = 'mongodb://<DarkGtr>:<1squall1>@ds054308.mongolab.com:54308/darknodeapp'
 var MongoClient = mongodb.MongoClient;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Connecting \n');
+        MongoClient.connect(url, function (err, db) {
+            response.write('Connection Made \n');
+            if (err) {
+                response.write('Unable to conect to the mongoDB server. Error:' + err + "\n");
+                db.close();
+            } else {
+                response.write('Connection established to' + url + "\n");
+                db.close();
+            }
+            response.end('Finished, Connection closed \n');
+            });
+        });
