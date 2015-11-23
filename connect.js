1d52155ba427220bf52ea3c217ba9e9170cea116
1400003 <1400003@rgu.ac.uk>
2015-11-23 16:29:17 +0000

    Hello

--- connect.js
+++ connect.js
@@ -4,7 +4,7 @@
 var mongodb = require ('mongodb');
 var http = require ('http');
 var port = process.env.PORT || 1337;
-var url = 'mongodb://<DarkGtr>:<1squall1>@ds054308.mongolab.com:54308/darknodeapp'
+var url = 'mongodb://DarkGtr:1squall1@ds054308.mongolab.com:54308/darknodeapp'
 var MongoClient = mongodb.MongoClient;
 
 /*http.createServer(function(request, response) {
