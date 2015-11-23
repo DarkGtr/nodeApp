8d2cd91ee84d9cfc6250512593ccd6036b2f9d4a
1400003 <1400003@rgu.ac.uk>
2015-11-23 17:13:20 +0000

    Hello

--- insert.js
+++ insert.js
@@ -17,12 +17,27 @@ http.createServer(function(request, response) {
             db.close();
         } else {
             response.write('Connection established to' + url +"\n");
-            db.close();
+            var collection = db.collection('users');
+
+            var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+            var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+            collection.insert([user1, user2, user3], function (err, result) {
+                if (err) {
+                    response.write('Insert failed ' + err + "\n");
+                } else {
+                    console.log(result);
+                    response.write('Inserted ' + result.insertedCount +' documents ok. +"\n"');
+                }
+                //Close connection
+                db.close();
+                response.end('Finished, Connection closed \n');
+                //remove any other db.close or response.end statement below this line
+            });
         }
-        response.end('Finished, Connection closed \n');
     });
 }).listen(port);
-
+/*
 var collection = db.collection('users');
 
 var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
@@ -34,10 +49,10 @@ collection.insert([user1, user2, user3], function (err, result){
         response.write('Insert failed ' + err + "\n");
     } else {
         console.log(result);
-        response.write('Inerted ' + result.insertedCount + ' documents ok. +"\n"');
+        response.write('Inserted ' + result.insertedCount + ' documents ok. +"\n"');
     }
         db.close();
         response.end('Finished, Connection closed \n');
 
-});
+});*/
 
