c59b60f388059770e1abfe6ca3be5ccd992e68b6
1400003 <1400003@rgu.ac.uk>
2015-11-23 15:05:21 +0000

    Hello

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-/**
-* Created by 1400003 on 23/11/2015.
-*/
-var http = require('http')
+/*Created by 1400003 on 23/11/2015.*/
+
+/*var http = require('http')
 var port = process.env.PORT || 1337;
 
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hello World!\n');
-}).listen(port);
+}).listen(port);*/
 
 var http = require('http')
 var port = process.env.PORT || 1337;
