e266f62c226673902fa4ff0428c31aef99f4e9c9
1400003 <1400003@rgu.ac.uk>
2015-11-30 17:29:34 +0000

    Hello

--- twittertest.js
+++ twittertest.js
@@ -13,7 +13,7 @@ var client = new Twitter ({
     access_token_secret: 'UzCAMrmrxIgv79KZfQVYVf5coump0SIDyhcRuI8MT29Nc'
 });
 http.createServer(function(request, response){
-   response.writeHead(200,{'Content-Type': 'text/plain'});
+   response.writeHead(200,{'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
 
     client.get('search/tweets', {q: 'lolcat'}, function(error, tweets){
         console.log(tweets);
