85bd8b1b2183488c0dad5e0e5363a35a33608e00
1400003 <1400003@rgu.ac.uk>
2015-11-30 16:29:28 +0000

    Hello

--- twittertest.js
+++ twittertest.js
@@ -3,14 +3,22 @@
  */
 
 var Twitter = require('twitter');
+var http = require('http')
+var port = process.env.PORT || 1337;
+
 var client = new Twitter ({
     consumer_key: 'FlHG5MRryB6gGBw7k19Enizaq',
     consumer_secret: 'j7LuAv3d5b8eCOqehJomhvi91BArqP2a5qtlFq5HsVma456bno',
     access_token_key: '4329333393-Bjg5j3KiVLwSTK34dWGMz0rePMEevO90HocrQpI',
     access_token_secret: 'UzCAMrmrxIgv79KZfQVYVf5coump0SIDyhcRuI8MT29Nc'
-})
-
-client.get('search/tweets', {q: 'lolcat'}, function(error, tweets){
-    console.log(tweets);
 });
+http.createServer(function(request, response){
+   response.writeHead(200,{'Content-Type': 'text/plain'});
+
+    client.get('search/tweets', {q: 'lolcat'}, function(error, tweets){
+        console.log(tweets);
+});
+
+
+}).listen(port);
 
